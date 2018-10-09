505483ddb3088a0da572c499d62bd98f2b8d7eb3
Volcan1c <john_balo@hotmail.com>
2018-10-09 20:39:20 +0200

    categories functionality

--- vending-front-end/src/app/components/product-list/product-list.component.ts
+++ vending-front-end/src/app/components/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductServiceService } from 'src/app/services/product-service.service';
+import { Product } from 'src/app/interfaces/product';
 
 @Component({
   selector: 'app-product-list',
@@ -10,8 +11,14 @@ export class ProductListComponent implements OnInit {
 
   constructor(public product: ProductServiceService) { }
   
+  displayItems: Array<Product> = this.product.mockUpData;
+
   testButton() {
     console.log(this.product.mockUpData);
+  }
+
+  categoryButton(category) {
+    this.displayItems = this.product.filterByCategory(category);
   }
 
   ngOnInit() {
